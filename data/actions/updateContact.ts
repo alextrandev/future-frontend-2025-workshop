8fdb61b666ba376b5c012edad5ed5507b2db92b9
Alex Tran <tranduclong092@gmail.com>
2025-05-26 16:02:12 +0300

    Feat: refactor ContactForm to use action state for form handling and add error component for better user feedback

--- data/actions/updateContact.ts
+++ data/actions/updateContact.ts
@@ -2,14 +2,24 @@
 
 import { redirect } from "next/navigation";
 import { prisma } from "@/db";
+import { slow } from "@/utils/slow";
 import { contactSchema } from "@/validations/contactSchema";
+import type { ContactSchemaType } from '@/validations/contactSchema';
 
 export async function updateContact(contactId: string, formData: FormData) {
   const data = Object.fromEntries(formData);
-  const result = contactSchema.parse(data);
+  const result = contactSchema.safeParse(data);
 
+  if (!result.success) {
+    return {
+      data: data as ContactSchemaType,
+      errors: result.error.formErrors,
+    };
+  }
+
+  await slow();
   await prisma.contact.update({
-    data: result,
+    data: result.data,
     where: {
       id: contactId,
     },
